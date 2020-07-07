ac7a7f790f472828bfdd8dec10bb181192ee9d57
Noelle Laureano <nrlaureano@gmail.com>
2020-07-07 18:34:42 -0400

    clean starting point

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react'
-// (2) second two ingredients from react router
 
 import './App.css'
 
 import { Bio, Experience, Projects, Contact } from './Components'
-import jobHistory from './Data/jobHistory'
 
 function App() {
   return (
@@ -12,20 +10,16 @@ function App() {
 
       <nav>
         <ul>
-          {/* (4) update your anchor tags to React Router Links (href >> to) */}
           <div className='nav-name'>
             <li><a href='/'>Noelle Laureano</a></li>
           </div>
           <div className='nav-menu'>
-            {/* (4) update links */}
             <li><a href='/projects'>Projects</a></li>
             <li><a href='/experience'>Experience</a></li>
             <li><a href='/contact'>Contact</a></li>
           </div>
         </ul>
       </nav>
-
-      {/* (3) Routes */}
 
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// (1) first ingredient from react router
 
 import './index.css'
 import App from './App'
 
-// (1) first ingredient from react router
 
 ReactDOM.render(
   <App />,
