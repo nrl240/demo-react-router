e31fe8bf810f9637c9aee6e51f7de9d3e636f420
Noelle Laureano <nrlaureano@gmail.com>
2020-07-07 17:46:12 -0400

    starting point

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 // (1) first ingredient from react router
 
 import './index.css'
@@ -7,7 +7,4 @@ import App from './App'
 
 // (1) first ingredient from react router
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
